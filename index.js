8b83fb16f3ae3773157a03f6162d750436cce357
Anthony Phan <89223273+anthphan@users.noreply.github.com>
2024-04-26 21:23:09 -0400

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./models/user");
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/', (req, res) =>  {
-    res.send({ hi: 'there'});
-});
+// cookie can last 30 days
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
 
-app.listen(3000);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
