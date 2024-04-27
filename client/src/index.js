05e6f2fcf7d9ca337b139e3d9ec7d87aadcfea61
Anthony Phan <89223273+anthphan@users.noreply.github.com>
2024-04-27 17:54:39 -0400

    Added routing logic to link auth

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'materialize-css/dist/css/materialize.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 1) Import ReactDOM library
+import ReactDOM from "react-dom/client";
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import {thunk} from 'redux-thunk';
+ 
+// 2) Import App component as usual
+import App from './components/App';
+import reducers from "./reducers";
+ 
+// 3) Get a reference to the div with ID root
+const el = document.getElementById("root");
+ 
+// 4) Tell React to take control of that element
+const root = ReactDOM.createRoot(el);
+const store = createStore(reducers, {}, applyMiddleware(thunk));
+ 
+// 5) Show the component on the screen
+root.render(<Provider store={store}><App /></Provider>);
